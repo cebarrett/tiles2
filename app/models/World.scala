bd228a004431e749e35123d1685d9b4b2bc4ce40
Chris Barrett <cbarrett@rocketlawyer.com>
2013-05-24 23:42:02 -0700

    fix bug

--- app/models/World.scala
+++ app/models/World.scala
@@ -9,29 +9,29 @@ class World {
 
 	def chunk(x:Int, y:Int, newChunk:Chunk = null):Chunk = {
 		require(0 <= x && x < World.length && 0 <= y && y < World.length)
-		var chunk = newChunk
-		if (chunk != null) {
-			chunks(Chunk.coord(x))(Chunk.coord(y)) = chunk;
+		var c = newChunk
+		if (c != null) {
+			chunks(Chunk.coord(x))(Chunk.coord(y)) = c;
 		} else {
-			chunk = chunks(Chunk.coord(x))(Chunk.coord(y));
+			c = chunks(Chunk.coord(x))(Chunk.coord(y));
 		}
 		/* chunks are lazy created */
-		if (chunk == null) {
+		if (c == null) {
 			chunks(Chunk.coord(x))(Chunk.coord(y)) = ChunkGenerator.generate(Chunk.coord(x),Chunk.coord(y))
-			chunk = ChunkGenerator.generate(Chunk.coord(x),Chunk.coord(y))
+			c = chunk(x,y)
 		}
-		chunk
+		c
 	}
 
 	def tile(x:Int, y:Int, newTile:Tile = null):Tile = {
 		require(0 <= x && x < Chunk.length && 0 <= y && y < Chunk.length)
-		var tile = newTile
-		if (tile != null) {
-			chunk(x,y).tile(x,y,tile)
+		var t = newTile
+		if (t != null) {
+			chunk(x,y).tile(x,y,t)
 		} else {
-			tile = chunk(x,y).tile(x,y)
+			t = chunk(x,y).tile(x,y)
 		}
-		tile
+		t
 	}
 
 }
