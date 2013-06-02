6c33e97032c1017ff374809ce03860ec098ae684
Chris Barrett <chris+github@cebarrett.net>
2013-06-02 16:13:09 -0700

    cleanup

--- app/models/Inventory.scala
+++ app/models/Inventory.scala
@@ -19,7 +19,6 @@ case class Inventory(var items:Seq[Item] = Seq.empty[Item]) {
 	}
 
 	def add(other:Item) = {
-		Logger.debug(s"add: $other")
 		if (other.count == None || items.filter({_.kind == other.kind}).isEmpty) {
 			items = items ++ Seq(other)
 		} else {
@@ -30,31 +29,23 @@ case class Inventory(var items:Seq[Item] = Seq.empty[Item]) {
 	}
 	
 	def subtract(other:Item):Boolean = {
-		Logger.debug(s"subtract: $other")
 		if (items.filter({_.kind == other.kind}).isEmpty) {
-Logger.debug("'''''empty")
 			return false
 		} else if (other.count == None) {
 			// TODO: Implement removing non-stackable items
-Logger.debug("''''' not stackable")
 			return false
 		} else {
-
-Logger.debug("''''' stackable, trying to subtract")
 			val existing:Option[Item] = items.filter({ item:Item => 
 				(item.kind == other.kind) && (item.count.get >= other.count.get)
 			}).headOption
 			if (existing.isDefined) {
 				val updated:Item = (existing.get - other).head
-				Logger.debug("***** updated count: " + updated.count.get)
 				items = items.filter({_.kind != other.kind})
 				if (updated.count.get > 0) {
 					items = items :+ updated
 				}
 				return true;
 			} else {
-
-Logger.debug("''''' ")
 				return false;
 			}
 		}
