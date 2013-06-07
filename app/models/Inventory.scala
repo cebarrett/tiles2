25d443fede4155af071d4928ad051624c3e6155c
Chris Barrett <cbarrett@rocketlawyer.com>
2013-06-07 13:00:37 -0700

    fixes

--- app/models/Inventory.scala
+++ app/models/Inventory.scala
@@ -8,7 +8,11 @@ import scala.util.control.Breaks._
 case class Inventory(var items:Seq[Item] = Seq.empty[Item], var selected:Option[Int] = None) {
 
 	// FIXME: for testing!
-	items = Item("pick", Some(1), Some(Wood)) +: items
+	items = Seq(
+		Item("pick", Some(1), Some(Wood)),
+		Item("stone", Some(100), Some(Stone.ORTHOCLASE)),
+		Item("workbench", Some(1))
+	)
 
 	/** Returns true if user has the given item (at least as many for item stacks) */
 	def has(other:Item):Boolean = {
@@ -29,10 +33,10 @@ case class Inventory(var items:Seq[Item] = Seq.empty[Item], var selected:Option[
 		other.count map { c:Int =>
 			if (c <= 0) return
 		}
-		if (other.count == None || items.filter({_.kind == other.kind}).isEmpty) {
+		if (other.count == None || items.filter({_.stacksWith(other)}).isEmpty) {
 			items = items ++ Seq(other)
 		} else if (other.count.get > 0) {
-			val existing:Item = items.filter({_.kind == other.kind}).head
+			val existing:Item = items.filter({_.stacksWith(other)}).head
 			val existingIndex:Int = items.indexOf(existing)
 			val updated:Item = (existing + other).get
 			items = items.patch(existingIndex, Seq(updated), 1)
@@ -43,32 +47,28 @@ case class Inventory(var items:Seq[Item] = Seq.empty[Item], var selected:Option[
 	 * Subtract an item. Handles item stacks and updating the selected item index.
 	 */
 	def subtract(other:Item):Boolean = {
-		if (items.filter({_.kind == other.kind}).isEmpty) {
-			return false
-		} else if (other.count == None) {
-			items = items.patch(items.indexOf(other), Seq(), 1)
-			return true
-		} else {
-			val existing:Option[Item] = items.filter({ item:Item => 
-				(item.kind == other.kind) && (item.count.get >= other.count.get)
-			}).headOption
-			val existingIndex = items.indexOf(existing.get)
-			if (existing.isDefined) {
-				val updated:Item = (existing.get - other).head
-				if (updated.count.get > 0) {
-					// replace
-					items = items.patch(existingIndex, Seq(updated), 1)
-				} else {
-					// subtract
-					items = items.filter({_.kind != other.kind})
-					if (selected != None && existingIndex >= selected.get) {
-						selected = Some(selected.get - 1)
-					}
-				}
-				return true;
+		// note: (for now) if the other item has no material it will
+		// subtract from the first item stack of the same kind.
+		items.filter({ item:Item =>
+			item.subtractableFrom(other)
+		}).headOption.map({ item:Item =>
+			val updated:Item = (item - other).head
+			if (updated.count.get > 0) {
+				// replace
+				items = items.patch(items.indexOf(item), Seq(updated), 1)
+				true
 			} else {
-				return false;
+				// subtract
+				items = items.filter({_ != item})
+				true
 			}
-		}
+		}).headOption.getOrElse({
+			if (other.count.isEmpty && items.contains(other)) {
+				items = items.patch(items.indexOf(other), Seq(), 1)
+				true
+			} else {
+				false
+			}
+		})
 	}
 }
--- app/models/Item.scala
+++ app/models/Item.scala
@@ -2,34 +2,37 @@ package models
 
 case class Item(val kind:String, val count:Option[Int] = None, val material:Option[Material] = None) {
 	def +(other:Item):Option[Item] = {
-		if (count == None || other.count == None) {
-			// count of None means an item is not stackable
-			return None
+		if (stacksWith(other)) {
+			Some(Item(kind, Some(count.head + other.count.head), material))
 		} else {
-			(kind, material) match {
-				// only stack the same kind/material of item
-				case (other.kind, other.material) => Some(Item(kind, Some(count.head + other.count.head), material))
-				case (_, _) => None
-			}
+			None
 		}
 	}
 	def -(other:Item):Option[Item] = {
-		if (count == None || other.count == None) {
-			// count of None means an item is not stackable
-			return None
-		} else {
-			(kind, material) match {
-				// only stack the same kind of item
-				case (other.kind, other.material) => {
-					val newCount:Int = count.head - other.count.head
-					if (newCount < 0) {
-						None
-					} else {
-						Some(Item(kind, Some(newCount), material))
-					}
-				}
-				case (_, _) => None
+		if (subtractableFrom(other)) {
+			val newCount:Int = count.head - other.count.head
+			if (newCount >= 0) {
+				Some(Item(kind, Some(newCount), material));
+			} else {
+				None
 			}
+		} else {
+			None
 		}
+	}
+	def stacksWith(other:Item):Boolean = {
+		return (
+			count.isDefined &&
+			other.count.isDefined &&
+			kind == other.kind &&
+			material == other.material
+		)
+	}
+	def subtractableFrom(other:Item):Boolean = {
+		return (
+			((count == None && other.count == None) || (count.isDefined && other.count.isDefined && (count.get >= other.count.get))) &&
+			(kind == other.kind) &&
+			( ! (other.material.isDefined && other.material != material) )
+		)
 	}
 }
