da984362222c051919b5414ced6ed25b2a589039
Chris Barrett <root@localhost>
2013-06-21 17:33:12 -0700

    cleanup

--- app/models/Direction.scala
+++ app/models/Direction.scala
@@ -5,19 +5,19 @@ sealed abstract trait Direction {
 	def y:Int
 }
 
-case object North extends Direction {
+object North extends Direction {
 	override val x =  0
 	override val y =  1
 }
-case object East  extends Direction {
+object East  extends Direction {
 	override val x =  1
 	override val y =  0
 }
-case object South extends Direction {
+object South extends Direction {
 	override val x =  0
 	override val y = -1
 }
-case object West  extends Direction {
+object West  extends Direction {
 	override val x = -1
 	override val y =  0
 }
