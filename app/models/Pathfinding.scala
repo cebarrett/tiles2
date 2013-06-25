994a8950abf0667d7138a7164d097ac767ac1857
Chris Barrett <chris+github@cebarrett.net>
2013-06-24 20:18:47 -0700

    very simple pathing method

--- app/models/Pathfinding.scala
+++ app/models/Pathfinding.scala
@@ -1,3 +1,18 @@
 package models
 
 import scala.collection.mutable.PriorityQueue
+
+case class Path(val dest:WorldCoordinates) {
+	def directionFrom(pos:WorldCoordinates):Direction = {
+		// go in the direction of the target w/o checking for obstacles
+		val (n, e, s, w) = (
+			WorldCoordinates(pos.x, pos.y+1).distanceTo(dest),
+			WorldCoordinates(pos.x+1, pos.y).distanceTo(dest),
+			WorldCoordinates(pos.x, pos.y-1).distanceTo(dest),
+			WorldCoordinates(pos.x-1, pos.y).distanceTo(dest))
+		if (n < e && n < s && n < w) North
+		else if (e < s && e < w) East
+		else if (s < w) South
+		else West
+	}
+}
