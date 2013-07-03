39e90f6d7e16ee65f0d5c617f8f004f998951eca
Chris Barrett <root@localhost>
2013-07-02 23:29:00 -0700

    wizards appear in lava

--- app/models/Structure.scala
+++ app/models/Structure.scala
@@ -14,9 +14,9 @@ case object StructureSpawn extends StructureGen {
 
 case object StructureWizard extends StructureGen {
 	def decorate(tile:Tile, pos:WorldCoordinates) = {
-		val T = if (Game.DEV) 50.00 else 150.0
-		val P = if (Game.DEV)  0.01 else   0.0004
-		if (tile.entity.isEmpty && math.abs(pos.x) > T && math.abs(pos.y) > T && Math.random < P) {
+		val T = 0
+		val P = if (Game.DEV) 0.05 else 0.0011
+		if (tile.terrain == TerrainLava && tile.entity.isEmpty && math.abs(pos.x) > T && math.abs(pos.y) > T && Math.random < P) {
 			tile.entity = Some(EntityWizard())
 		}
 	}
